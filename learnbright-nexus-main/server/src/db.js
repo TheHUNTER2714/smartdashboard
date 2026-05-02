25ec8520f4bd1696ea561f902d5df0ae1801c9f3
Ayush Agnihotri <ayushagnihotri964@gmail.com>
2026-05-02 08:22:39 +0530

    Update db.js

--- learnbright-nexus-main/server/src/db.js
+++ learnbright-nexus-main/server/src/db.js
@@ -5,8 +5,6 @@ dotenv.config();
 
 const { Pool } = pg;
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
